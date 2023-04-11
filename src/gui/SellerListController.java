154b9e4acc0c46cc46c75162ea1c8c50b08c8a90
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-04-11 19:16:41 -0300

    SellerForm

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,6 @@
 package gui;
 
-//import java.io.IOException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -17,9 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-//import javafx.scene.Scene;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -27,8 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-//import javafx.scene.layout.Pane;
-//import javafx.stage.Modality;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -105,9 +105,8 @@ public class SellerListController implements Initializable, DataChangeListener {
         initRemoveButtons();
     }
 
-     
     private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-       /* try {
+        try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
 
@@ -127,8 +126,8 @@ public class SellerListController implements Initializable, DataChangeListener {
 
         } catch (IOException e) {
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-        }*/
-    } 
+        }
+    }
 
     @Override
     public void onDataChange() {
@@ -177,18 +176,17 @@ public class SellerListController implements Initializable, DataChangeListener {
     private void removeEntity(Seller obj) {
         Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
 
-        if(result.get() == ButtonType.OK){
-            if(service == null){
+        if (result.get() == ButtonType.OK) {
+            if (service == null) {
                 throw new IllegalStateException("Service was null");
             }
-            try{
+            try {
                 service.remove(obj);
                 updateTableView();
-            }
-            catch(DbException e){
+            } catch (DbException e) {
                 Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
             }
-            
+
         }
     }
 
